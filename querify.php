5bdd8e3d364d9a7efca10814fa65657e4395399c
Abukari Einus <40009807+blackdante101@users.noreply.github.com>
2021-02-05 11:05:52 +0000

    Add files via upload

---
+++ querify.php
@@ -0,0 +1,50 @@
+<?php 
+ class Querify 
+{
+	public $db;
+	public function connect($servername,$username,$password,$databasename)
+	{
+		$this->db = new mysqli($servername,$username,$password,$databasename);
+		if($this->db->connect_error)
+		{
+			echo "connection error".$this->db->connect_error;
+		}
+	}
+	public function SelectAll($tblname)
+	{
+		$stmt=$this->db->prepare("SELECT * FROM $tblname");
+		$stmt->execute();
+		return $stmt->get_result();
+		$stmt->close();
+	}
+	public function Delete($tblname,$id)
+	{
+		$stmt=$this->db->prepare("DELETE FROM $tblname WHERE id=?");
+		$stmt->bind_param('i',$id);
+		$stmt->execute();
+		$stmt->close();
+	}
+	public function Search($tblname,$column,$data)
+	{
+		$stmt=$this->db->prepare("SELECT * FROM $tblname WHERE $column LIKE ? ");
+		$query="%".$data."%";
+		$stmt->bind_param('s',$query);
+		$stmt->execute();
+		return $stmt->get_result();
+		$stmt->close();
+	}
+	public function Insert($tblname,$array)
+	{
+		$values = implode(',',array_values($array));
+		$columns = implode(',',array_keys($array));
+		$stmt=$this->db->prepare("INSERT INTO $tblname($columns) VALUES (?)");
+		$stmt->bind_param('s',$values);
+		$stmt->execute();
+		$stmt->close();
+
+	}
+
+
+}
+
+?>
